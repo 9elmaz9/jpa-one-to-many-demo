0b5f1a0e33863da1d6fe67f65096f2faa4b3c801
9elmaz9 <kakbyelmaz@gmail.com>
2024-05-15 15:49:50 +0200

    new

--- src/main/java/be/intecbrussel/jpaonetomanydemo/SpringDemoApplication.java
+++ src/main/java/be/intecbrussel/jpaonetomanydemo/SpringDemoApplication.java
@@ -2,12 +2,14 @@ package be.intecbrussel.jpaonetomanydemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
-public class JpaOneToManyDemoApplication {
+public class SpringDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JpaOneToManyDemoApplication.class, args);
+        SpringApplication.run(SpringDemoApplication.class, args);
     }
 
 }
--- src/test/java/be/intecbrussel/jpaonetomanydemo/JpaOneToManyDemoApplicationTests.java
+++ src/test/java/be/intecbrussel/jpaonetomanydemo/JpaOneToManyDemoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JpaOneToManyDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
